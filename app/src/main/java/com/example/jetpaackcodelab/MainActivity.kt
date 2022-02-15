5a5501e0cae9bd1e4b1be5851c8cc11876ff7a2e
Nikita <guschinnick@gmail.com>
2022-02-15 20:39:32 +0300

    lesson 08

--- app/src/main/java/com/example/jetpaackcodelab/MainActivity.kt
+++ app/src/main/java/com/example/jetpaackcodelab/MainActivity.kt
@@ -4,12 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -20,7 +17,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackCodelabTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
                     MyApp()
                 }
@@ -30,26 +26,67 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-private fun MyApp(names: List<String> = mutableStateListOf("Word", "Compose")){
-    Column (modifier = Modifier.padding(4.dp)){
-        for(name in names){
+fun MyApp() {
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+    if (shouldShowOnboarding) {
+        OnboardingScreen { shouldShowOnboarding = false }
+    } else {
+        Greetings()
+    }
+}
+
+@Composable
+private fun Greetings(names: List<String> = mutableStateListOf("Word", "Compose")) {
+    Column(modifier = Modifier.padding(4.dp)) {
+        for (name in names) {
             Greeting(name = name)
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    JetpackCodelabTheme {
+        OnboardingScreen {}
+    }
+}
+
+@Composable
+fun OnboardingScreen(onClick: () -> Unit) {
+    Surface {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Welcome to Codelabs!")
+            Button(
+                modifier = Modifier.padding(vertical = 24.dp),
+                onClick = { onClick.invoke() }
+            ) {
+                Text("Continue")
+            }
         }
     }
 }
 
 @Composable
 fun Greeting(name: String) {
-    var expanded = false
+    val expanded = remember { mutableStateOf(false) }
+
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
 
     Surface(color = MaterialTheme.colors.primary, modifier = Modifier.padding(8.dp, 4.dp)) {
         Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier.weight(1f)) {
+            Column(modifier = Modifier
+                .weight(1f)
+                .padding(bottom = extraPadding)) {
                 Text(text = "Hello,")
                 Text(text = "$name!")
             }
-            OutlinedButton(onClick = { expanded = !expanded}) {
-                Text(if(expanded)"Show more" else "Show less")
+            OutlinedButton(onClick = { expanded.value = !expanded.value }) {
+                Text(if (expanded.value) "Show less" else "Show more")
             }
         }
     }
@@ -59,6 +96,6 @@ fun Greeting(name: String) {
 @Composable
 fun TextPreview() {
     JetpackCodelabTheme {
-        MyApp()
+        Greetings()
     }
 }
